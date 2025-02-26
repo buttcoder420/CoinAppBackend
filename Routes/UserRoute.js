7be6559e48d6b0f1519e11ced19252e399f43167
Coder Bhai <yadspakistan.pk.@gmail.com>
2025-02-26 07:29:43 +0500

    Update UserRoute.js

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -4,8 +4,14 @@ const {
   loginController,
   registerController,
   requireSign,
+
   getUserBalance,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
 } = require("../Controller/UserController");
+const { IsAdmin } = require("../Controller/DailyRewardController");
 
 const router = express.Router();
 
@@ -17,5 +23,15 @@ router.post("/login", loginController);
 
 //get coin aount
 router.get("/balance", requireSign, getUserBalance);
+// Get all users (Admin only)
+router.get("/all-users", requireSign, IsAdmin, getAllUsers);
+
+// Get specific user details
+router.get("/get-single/:id", requireSign, IsAdmin, getUserById);
+// Update user (Admin only)
+router.put("/update-user/:id", requireSign, IsAdmin, updateUser);
+
+// Delete user (Admin only)
+router.delete("/delete user/:id", requireSign, IsAdmin, deleteUser);
 
 module.exports = router;
