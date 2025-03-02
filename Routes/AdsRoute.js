9b304016836549897bdc956aa8f1a867283b63ac
Coder Bhai <yadspakistan.pk.@gmail.com>
2025-03-02 22:05:01 +0500

    Update AdsRoute.js

--- Routes/AdsRoute.js
+++ Routes/AdsRoute.js
@@ -1,7 +1,14 @@
 const express = require("express");
-const { watchAd, requireSign } = require("../Controller/AdsController");
+const {
+  watchAd,
+  requireSign,
+  IsAdmin,
+  getUserAdStats,
+} = require("../Controller/AdsController");
 const router = express.Router();
 
 router.post("/watch-ad", requireSign, watchAd);
+
+router.get("/user-ad-stats", requireSign, IsAdmin, getUserAdStats);
 
 module.exports = router;
