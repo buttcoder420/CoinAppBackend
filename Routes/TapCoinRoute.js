cd14730e543e089fe5274560d2754da68a27cbc1
Coder Bhai <yadspakistan.pk.@gmail.com>
2025-03-02 11:24:18 +0500

    Update TapCoinRoute.js

--- Routes/TapCoinRoute.js
+++ Routes/TapCoinRoute.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const {
   requireSign,
-  getRemainingLimit,
   addCoinTap,
+  getRemainingLimit,
   resetDailyLimit,
-} = require("../Controller/TapCoinController");
+} = require("../Controller/CoinTapController");
 
 const router = express.Router();
 
