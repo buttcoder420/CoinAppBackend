9b3fe8623351e2f3f163d7bad3c4eb0ee0b54832
Coder Bhai <yadspakistan.pk.@gmail.com>
2025-03-04 16:41:37 +0500

    Add files via upload

---
+++ Routes/CashOutRoute.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const {
+  requestCashOut,
+  requireSign,
+  getAllCashOutRequests,
+  updateCashOutStatus,
+  IsAdmin,
+  deleteCashOutRequest,
+} = require("../Controller/CashOutController");
+const router = express.Router();
+
+// Route for user to request cash-out
+router.post("/cash-out", requireSign, requestCashOut);
+
+// Routes for admin to manage cash-out requests
+router.get("/cash-out", requireSign, getAllCashOutRequests);
+router.put(
+  "/cash-out/update-status",
+  requireSign,
+  IsAdmin,
+  updateCashOutStatus
+);
+router.delete(
+  "/admin/cash-out/:id",
+  requireSign,
+  IsAdmin,
+  deleteCashOutRequest
+);
+
+module.exports = router;
